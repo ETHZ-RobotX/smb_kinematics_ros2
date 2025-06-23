b6ddcfab869bce55d8b1c9c34bf7b57afc7845b6
RobotX-SMB <supermegabot@ethz-asl.ch>
2025-06-23 13:43:40 +0200

    required tools for graph msf + far + tare

--- src/global_to_local_odometry.cpp
+++ src/global_to_local_odometry.cpp
@@ -22,9 +22,9 @@ public:
       tf_buffer_(this->get_clock()),
       tf_listener_(tf_buffer_)
     {
-        this->declare_parameter("topic_name", "/graph_msf/est_odometry_world_imu");
+        this->declare_parameter("topic_name", "/state_estimation");
         this->declare_parameter("use_ground_truth", false);
-        this->declare_parameter("output_topic", "/state_estimation");
+        this->declare_parameter("output_topic", "/local_odometry");
 
         use_ground_truth_ = this->get_parameter("use_ground_truth").as_bool();
 
@@ -44,7 +44,7 @@ public:
             10);
 
         // PointCloud transformation if using ground truth
-        if (true) {
+        if (use_ground_truth_) {
             pointcloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
                 "/rslidar/points",
                 10,
@@ -58,90 +58,64 @@ public:
     }
 
 private:
-    // void odometryCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
-    // {
-
-    //     if (true) {
-    //         msg.header.frame_id = "odom";
-    //         local_odometry_pub_->publish(*msg);
-    //         return; // If using ground truth, just publish the original message, we dont need to transform it
-    //     }
-
-    //     tf2::Quaternion quat(
-    //         msg->pose.pose.orientation.x,
-    //         msg->pose.pose.orientation.y,
-    //         msg->pose.pose.orientation.z,
-    //         msg->pose.pose.orientation.w);
-
-    //     tf2::Vector3 linear_velocity(
-    //         msg->twist.twist.linear.x,
-    //         msg->twist.twist.linear.y,
-    //         msg->twist.twist.linear.z);
-
-    //     tf2::Vector3 angular_velocity(
-    //         msg->twist.twist.angular.x,
-    //         msg->twist.twist.angular.y,
-    //         msg->twist.twist.angular.z);
-
-    //     tf2::Vector3 local_linear_velocity = tf2::quatRotate(quat.inverse(), linear_velocity);
-    //     tf2::Vector3 local_angular_velocity = tf2::quatRotate(quat.inverse(), angular_velocity);
-
-    //     nav_msgs::msg::Odometry local_odometry;
-    //     local_odometry.header = msg->header;
-    //     local_odometry.child_frame_id = msg->child_frame_id;
-    //     local_odometry.pose.pose = msg->pose.pose;
-    //     local_odometry.twist.twist.linear.x = local_linear_velocity.x();
-    //     local_odometry.twist.twist.linear.y = local_linear_velocity.y();
-    //     local_odometry.twist.twist.linear.z = local_linear_velocity.z();
-    //     local_odometry.twist.twist.angular.x = local_angular_velocity.x();
-    //     local_odometry.twist.twist.angular.y = local_angular_velocity.y();
-    //     local_odometry.twist.twist.angular.z = local_angular_velocity.z();
-
-    //     local_odometry_pub_->publish(local_odometry);
-    // }
-
     void odometryCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
-        // Only transform if using ground trut
-        try {
-            geometry_msgs::msg::TransformStamped transform_stamped =
-                tf_buffer_.lookupTransform(
-                    msg->child_frame_id,  
-                    "rslidar",  
-                    msg->header.stamp,
-                    rclcpp::Duration::from_seconds(0.1)
-                );
 
-            // sensor_msgs::msg::PointCloud2 transformed_cloud;
-            nav_msgs::msg::Odometry transformed_odometry;
-            tf2::doTransform(msg->pose.pose, transformed_odometry.pose.pose, transform_stamped);
-            
-            transformed_odometry.child_frame_id = "rslidar";
-            transformed_odometry.header.frame_id = "odom";
-
-            local_odometry_pub_->publish(transformed_odometry);
-        } catch (const tf2::TransformException &ex) {
-            RCLCPP_WARN(this->get_logger(), "Could not transform odometry: %s", ex.what());
+        if (use_ground_truth_) {
+            local_odometry_pub_->publish(*msg);
+            return; // If using ground truth, just publish the original message, we dont need to transform it
         }
+
+        tf2::Quaternion quat(
+            msg->pose.pose.orientation.x,
+            msg->pose.pose.orientation.y,
+            msg->pose.pose.orientation.z,
+            msg->pose.pose.orientation.w);
+
+        tf2::Vector3 linear_velocity(
+            msg->twist.twist.linear.x,
+            msg->twist.twist.linear.y,
+            msg->twist.twist.linear.z);
+
+        tf2::Vector3 angular_velocity(
+            msg->twist.twist.angular.x,
+            msg->twist.twist.angular.y,
+            msg->twist.twist.angular.z);
+
+        tf2::Vector3 local_linear_velocity = tf2::quatRotate(quat.inverse(), linear_velocity);
+        tf2::Vector3 local_angular_velocity = tf2::quatRotate(quat.inverse(), angular_velocity);
+
+        nav_msgs::msg::Odometry local_odometry;
+        local_odometry.header = msg->header;
+        local_odometry.child_frame_id = msg->child_frame_id;
+        local_odometry.pose.pose = msg->pose.pose;
+        local_odometry.twist.twist.linear.x = local_linear_velocity.x();
+        local_odometry.twist.twist.linear.y = local_linear_velocity.y();
+        local_odometry.twist.twist.linear.z = local_linear_velocity.z();
+        local_odometry.twist.twist.angular.x = local_angular_velocity.x();
+        local_odometry.twist.twist.angular.y = local_angular_velocity.y();
+        local_odometry.twist.twist.angular.z = local_angular_velocity.z();
+
+        local_odometry_pub_->publish(local_odometry);
     }
 
     void pointcloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
     {
         // Only transform if using ground truth
+        if (!use_ground_truth_) return;
 
+        std::string target_frame = "odom";
         try {
             geometry_msgs::msg::TransformStamped transform_stamped =
                 tf_buffer_.lookupTransform(
-                    "world_graph_msf",  
-                    msg->header.frame_id, 
+                    "odom",  // target frame
+                    msg->header.frame_id,  // source frame
                     msg->header.stamp,
                     rclcpp::Duration::from_seconds(0.1)
                 );
 
             sensor_msgs::msg::PointCloud2 transformed_cloud;
             tf2::doTransform(*msg, transformed_cloud, transform_stamped);
-
-            transformed_cloud.header.frame_id = "odom";
 
             pointcloud_pub_->publish(transformed_cloud);
         } catch (const tf2::TransformException &ex) {
